8fe925a58d348570395f456907a01f8c802b59f7
ibraheem <ibrahimaziz719@gmail.com>
2023-08-27 01:01:37 -0400

    add published_at for blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -16,7 +16,7 @@ class BlogPostsController < ApplicationController
   end
 
   def index
-    @blogs = BlogPost.all
+    @blogs = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
@@ -41,11 +41,11 @@ class BlogPostsController < ApplicationController
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
